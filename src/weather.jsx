69f4dd0da54fe2b586d28d18be14df2386712bbc
Guhan Manickavasagam <mguhan1999@gmail.com>
2024-12-31 15:08:32 +0530

    update

--- src/weather.jsx
+++ src/weather.jsx
@@ -31,10 +31,10 @@ function Weather() {
 
     return (
 
-        <div className="bg-blue-950 p-16 m-10 rounded-md   md:flex  gap-32">
+        <div className="bg-blue-950 p-16 rounded-md   md:flex  gap-">
 
             <div className="p-16 flex flex-col gap-2">
-                <div>
+                <div className="text-center">
                     <h1 className="text-3xl text-green-500 font-bold ">Weather </h1><p className="text-3xl underline text-white"> app  ☾☼</p>
                 </div>
 
@@ -43,12 +43,12 @@ function Weather() {
 
             </div>
 
-            <div className="l-screen bg-gradient-to-br from-blue-500 via-blue-600 to-pink-500  p-10 flex-grow border-y-white rounded-md my-20">
+            <div className="l-screen bg-gradient-to-br from-blue-500 via-blue-600 to-pink-500  p-10 flex-grow border-y-white rounded-md m- md:my-20 ">
                 <h1 className="text-center  font-bold md:flex text-2xl text-white">Weather Report</h1>
                 <p className="text-center md:flex text-white">I can give you a weather report about your city!</p>
 
                 <div className="flex flex-col">
-                    <input  onChange={handlecity}  type="text" name="q" class="  border h-12 shadow p-4 rounded-full dark:text-gray-800 dark:border-gray-700 dark:bg-gray-200 my-4  w-48 md:w-56" placeholder="search weather.....☾☼"></input>
+                    <input  onChange={handlecity}  type="text" name="q" class="  border h-12 shadow p-4 rounded-full dark:text-gray-800 dark:border-gray-700 dark:bg-gray-200 my-4  w-full md:w-52" placeholder="search weather.....☾☼"></input>
 
                     <button  onClick={getweather}  className="text-left  bg-blue-950 text-white w-fit p-1 mx-2 rounded-md">Get 🌤 Report</button>
                 </div>
